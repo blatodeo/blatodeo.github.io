6521d6c15c41332e787f85f8ef20dd7f08dc8251
blatodeo <nicojaviguato@gmail.com>
2023-04-29 11:07:51 -0500

    finally weights showing on formula, but another problem appear

--- producto/detalles_producto.php
+++ producto/detalles_producto.php
@@ -114,26 +114,27 @@ $descripcion_producto = $_GET['descripcion_producto'];
       formula.id,
       materia_prima.codigo,
       materia_prima.descripcion,
-      materia_prima.peso,
+      peso.peso AS valor,
       (SELECT precio FROM precio_mp WHERE precio_mp.mp = materia_prima.codigo AND linea_precio = 6) AS precio,
       formula.peso AS peso 
     FROM formula 
     INNER JOIN materia_prima ON formula.codigo_mp = materia_prima.codigo
+    INNER JOIN peso ON formula.peso = peso.id
     WHERE codigo_producto = $codigo";
 
 $dato = mysqli_query($conexion, $SQL);
 if ($dato->num_rows > 0) {
   while ($fila = mysqli_fetch_array($dato)) {
-    $totalPeso += $fila['peso'];
+    $totalPeso += $fila['valor'];
     $totalPrecio += $fila['precio'];
-    $costoMP = $fila['precio'] * $fila['peso'];
+    $costoMP = $fila['precio'] * $fila['valor'];
     $totalCostoMP += $costoMP;
 ?>
   <tr>
     <td><?php echo $fila['id']; ?></td>
     <td><?php echo $fila['codigo'] . ' - ' . $fila['descripcion'] ?> </td>
     <td><?php echo '$' . $fila['precio']; ?></td>
-    <td><?php echo $fila['peso']; ?><a class="btn btn-warning" href="cambiar_peso.php?id=<?php echo $fila['id'] ?>&codigo_producto=<?php echo $codigo; ?>&descripcion_producto=<?php echo $descripcion_producto ?>"
+    <td><?php echo $fila['valor']; ?><a class="btn btn-warning" href="cambiar_peso.php?id=<?php echo $fila['id'] ?>&codigo_producto=<?php echo $codigo; ?>&descripcion_producto=<?php echo $descripcion_producto ?>"
 >
                   <i class="fas fa-pencil-alt"></i></a>
 
