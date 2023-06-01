0af7f9bcbd720b8534355fc67f56b1911886b81a
blatodeo <nicojaviguato@gmail.com>
2023-06-01 09:05:40 -0500

    Excel import with formula code conditions working

--- producto/importar_excel.php
+++ producto/importar_excel.php
@@ -51,11 +51,12 @@ if (isset($_FILES['archivo_excel'])) {
   // Recorrer todas las filas del archivo Excel y buscar coincidencia del código de gestión
   for ($row = 2; $row <= $highestRow; $row++) {
     $codigo_gestion = $worksheet->getCell('B' . $row)->getValue();
-    echo $codigo_gestion;
 
     if ($codigo_gestion == $codigo) {
       $encontrado = true;
       echo $codigo;
+      echo $codigo_gestion;
+
 
       break;
     }
@@ -63,8 +64,8 @@ if (isset($_FILES['archivo_excel'])) {
 
   if ($encontrado) {
     // Procesar las filas de la segunda tabla
-    $composicion_start_row = $row + 5; // Fila donde comienza la segunda tabla
-    $composicion_end_row = $composicion_start_row + 19; // Fila donde termina la segunda tabla
+    $composicion_start_row = $row + 9; // Fila donde comienza la segunda tabla
+    $composicion_end_row = $composicion_start_row + 30; // Fila donde termina la segunda tabla
 
         for ($row = $composicion_start_row; $row <= $composicion_end_row; $row++) {
             $codigo_mp = $worksheet->getCell('A' . $row)->getValue();
